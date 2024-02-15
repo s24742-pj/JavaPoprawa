83d62ad8c124b7d3f65277189f8a64bb55afac8a
s24742-pj <92209016+s24742-pj@users.noreply.github.com>
2024-02-15 18:56:40 +0100

    Add files via upload
    utworzenie projektu dodanie lombok i swagger

---
+++ s24742_LibraryManagement/src/main/java/com/example/s24742_librarymanagement/S24742LibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.s24742_librarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S24742LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S24742LibraryManagementApplication.class, args);
+    }
+
+}
